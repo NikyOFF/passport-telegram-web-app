641e7ff098a909ed4f7d6f346356b4587bcba6bf
NikyOFF <67484909+NikyOFF@users.noreply.github.com>
2023-07-26 12:25:41 +0600

    1.1.0

--- src/extract-data.class.ts
+++ src/extract-data.class.ts
@@ -5,13 +5,13 @@ export class ExtractData {
     const authDate = request.headers["tg-web-app-auth-date"];
     const queryId = request.headers["tg-web-app-query-id"];
     const userRaw = request.headers["tg-web-app-user"];
-    
+
     if (!(authDate && queryId && userRaw)) {
       return null;
     }
 
     const userRawDecoded = decodeURIComponent(userRaw.toString());
-    
+
     return {
       authDate: authDate as string,
       queryId: queryId as string,
